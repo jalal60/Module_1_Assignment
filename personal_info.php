6e2c1a3162729cf6a491b860525aafbeb4219f3d
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-11 21:22:08 +0600

    Add files via upload

---
+++ personal_info.php
@@ -0,0 +1,7 @@
+<?php
+$name="Md. Shahjalal";
+$age=36;
+$country="Bangladesh";
+$introduction="Hi, I am Md. Shahjalal. I am service holder(Banker). I Completed my graduate in 2009 and servicing Corporate level job since 2010. I have a family with my wife and two beautiful children. Though it's too late at this age 36 to learn a new technology but it's easy for me becasue my background was IT related. But in Job sector i don't need to develop any solution/software. That's why I am Interest to create a career in Software solution specially Web Development and enrolled Ostad platform for learning PHP Laravel Web Development. I have a high vision and long planning in Web Developent sector. It's just starting...... Thanks.";
+
+echo "Name=".$name."\nAge=".$age."\nCountry=".$country."\nBrif Description About me=".$introduction;
